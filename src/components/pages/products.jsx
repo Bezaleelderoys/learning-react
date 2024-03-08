8353e99b11b5dc5f59789d621802f0016354d317
Bezaleelderoys <bezaleelelderoy@gmail.com>
2024-03-08 11:24:09 +0800

    event handler

--- src/components/pages/products.jsx
+++ src/components/pages/products.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import Button from "../Elements/Button";
 import CardProduct from "../Fragments/CardProduct";
 
@@ -28,19 +29,35 @@ const products = [
   },
 ];
 
+const email = localStorage.getItem("email");
+
 const ProductPage = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
+
   return (
-    <div className="flex justify-center py-5">
-      {products.map((product) => (
-        <CardProduct>
-          <CardProduct.Header image={product.image}></CardProduct.Header>
-          <CardProduct.Body name={product.name}>
-            {product.description}
-          </CardProduct.Body>
-          <CardProduct.Footer price={product.price}></CardProduct.Footer>
-        </CardProduct>
-      ))}
-    </div>
+    <Fragment>
+      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+        {email}
+        <Button variant="ml-5 bg-black" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image}></CardProduct.Header>
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price}></CardProduct.Footer>
+          </CardProduct>
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
